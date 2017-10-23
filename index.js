483a9f777afeba463cab7e58ed30c001477a14eb
Kyogo Mochida <kyogo@jp.ibm.com>
2017-10-23 22:04:55 +0900

    define main process

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-var chalk       = require('chalk');
-console.log(chalk.red('hello note'));
+#!/usr/bin/env node
+'use strict';
+var chalk = require('chalk');
+var dateUtils = require('date-utils');
+var file  = require('./lib/file.js');
+var method = process.argv[2];
+var args1 = process.argv[3];
+var args2 = process.argv[4];
+
+switch(method) {
+  case 'list':
+    file.readFile('titles');
+    break;
+  case 'list -a':
+    file.readFile('all');
+    break;
+  case 'add':
+    file.appendFile(args1, args2);
+    break;
+  case 'help':
+    console.log('this is how to use todo_cli...');
+    console.log('');
+    console.log('list                      show all titles of todo');
+    console.log('list -a                   show all infomation of todo');
+    console.log('add [title] [detail]      add todo');
+    console.log('show last                 show todo most recently added');
+    console.log('show first                show todo most primary added');
+    console.log('delete last               delete todo most recently added');
+    console.log('delete first              delete todo most primary added');
+    console.log('delete -a                 delete all todo');
+    console.log('move [number] to [number] move todo');
+    break;
+}
