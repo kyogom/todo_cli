bedb922d0690b5610c4f5c877b0e25b8eb107acc
Kyogo Mochida <kyogo@jp.ibm.com>
2017-10-25 20:47:15 +0900

    refactor help & fix list options

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict';
 var dateUtils = require('date-utils');
 var file  = require('./lib/file.js');
+var chalk = require('chalk');
 var method = process.argv[2];
 var args1 = process.argv[3];
 var args2 = process.argv[4];
@@ -9,25 +10,22 @@ var args2 = process.argv[4];
 switch(method) {
   case 'list':
     if(!args1) {
+      //引数がなければタイトルのみ一覧表示する
       file.readFile('titles');
-    } else if(args1 ==='-a') {
+    } else if(args1 ==='-a' || args1 ==='all') {
+      //オプション「-a」か「all」があれば、全てのTodoを一覧表示する
       file.readFile('all');
     }
     break;
   case 'add':
-    file.appendFile(args1, args2);
+    if(args2){
+      file.appendFile(args1, args2);
+    } else {
+      args2 = ' ';
+      file.appendFile(args1, args2);
+    }
     break;
-  case 'help':
-    console.log('this is how to use todo_cli...');
-    console.log('');
-    console.log('list                      show all titles of todo');
-    console.log('list -a                   show all infomation of todo');
-    console.log('add [title] [detail]      add todo');
-    console.log('show last                 show todo most recently added');
-    console.log('show first                show todo most primary added');
-    console.log('delete last               delete todo most recently added');
-    console.log('delete first              delete todo most primary added');
-    console.log('delete -a                 delete all todo');
-    console.log('move [number] to [number] move todo');
+  default:
+      file.readHelp();
     break;
 }
